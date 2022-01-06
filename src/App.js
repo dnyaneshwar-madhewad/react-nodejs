21bf2c49121c88c11e6d59edcc5050bb90ae67a4
dnyaneshwar-madhewad <itz.dnyaneshwar@gmail.com>
2022-01-06 21:07:46 +0530

    project1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row bg-dark text-light p-3">
+        <div className="col h3 ">React</div>
+      </div>
+
+      <div className="row">
+        <div className="col-12 bg-secondary text-light p-3">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eos
+          fugiat recusandae accusamus? Illum odit ipsa rem labore officiis,
+          fugiat dignissimos praesentium doloribus delectus similique soluta
+          deserunt! Debitis, expedita facere!
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
